7319e246fca5901809a6ed6b94bc611456aad078
Vitor-W-Espindola <vitor.w@edu.pucrs.br>
2021-09-12 21:47:21 -0300

    Views and urls

--- FeathersFightApp/views.py
+++ FeathersFightApp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("All fighting styles page.")
+
+def fight(request, fight_id):
+    return HttpResponse("A fight page -> %s" % fight_id)
