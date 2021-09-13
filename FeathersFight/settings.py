519623ea57d3f974a23cc0b91339f178d2fd9276
Vitor-W-Espindola <vitor.w@edu.pucrs.br>
2021-09-12 22:09:48 -0300

    Creates fight model and does migration

--- FeathersFight/settings.py
+++ FeathersFight/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'FeathersFightApp.apps.FeathersfightappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- FeathersFightApp/models.py
+++ FeathersFightApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Fight(models.Model):
+    style = models.CharField(max_length=30)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
