4091db2c5791e3b198bb28dbf48f4ecfa575c606
Vitor-W-Espindola <vitor.w@edu.pucrs.br>
2021-09-12 23:37:22 -0300

    Improves response from database

--- FeathersFightApp/views.py
+++ FeathersFightApp/views.py
@@ -7,10 +7,21 @@ from .models import Fight
 # Create your views here.
 
 def index(request):
+
     fights = Fight.objects.all()
+    class FightWithShortDescription():
+        def __init__(self, fight):
+            self.fight = fight
+            self.short_description = "%s%s" % (' '.join(fight.description.split(" ")[:7]), "...")            
+     
+    fights_with_short_description = []
+
+    for fight in fights:
+        fights_with_short_description.append(FightWithShortDescription(fight))
+    
     template = loader.get_template('FeathersFightApp/index.html')
     context = {
-        'fights_list':fights,
+        'fights_list':fights_with_short_description,
     }
     return HttpResponse(template.render(context, request))
 
